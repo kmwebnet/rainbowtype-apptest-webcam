820d3731d52fea98243f0d339d707db73256c7f3
kmwebnet <46954791+kmwebnet@users.noreply.github.com>
2020-11-12 06:30:42 +0900

    rebuild js

--- src/components/sub-component.tsx
+++ src/components/sub-component.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import 'xterm/css/xterm.css';
 
 
