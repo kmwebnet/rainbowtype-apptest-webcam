c06612db76a63197727de18c9887b5533d7d61ba
kmwebnet <46954791+kmwebnet@users.noreply.github.com>
2020-12-07 16:00:13 +0900

    update test

--- src/components/sub-component2.tsx
+++ src/components/sub-component2.tsx
@@ -5,7 +5,7 @@ function SubComponent2() {
   const wsUrl = 'wss://' + window.location.host + '/ws';
   const ws = useRef(new WebSocket(wsUrl));
   ws.current.binaryType = 'arraybuffer';
-  const imgref = useRef<Array<React.RefObject<HTMLImageElement>>>([]);
+  const imgref = useRef<Array<HTMLImageElement>>([]);
   const serial: string[] = [];
   const [slen, setslen] = useState<number>(0);
 
@@ -22,7 +22,7 @@ function SubComponent2() {
           }
           const idx = serial.indexOf(tserial);
           console.log(idx);
-          const cref = imgref.current[idx].current;
+          const cref = imgref.current[idx];
           if (cref && cref.complete) {
             cref.src =
               'data:image/jpg;base64,' +
@@ -37,9 +37,7 @@ function SubComponent2() {
   useEffect(() => () => ws.current.close(), [ws]);
 
   useEffect(() => {
-    imgref.current = Array(slen)
-      .fill(0)
-      .map((_, i) => imgref.current[i] || createRef());
+    imgref.current = new Array(slen);
   }, [slen]);
 
   return (
@@ -47,7 +45,11 @@ function SubComponent2() {
       <h2>{window.location.host}</h2>
       {serial.map((v, i) => (
         <div key={v}>
-          <img key={v} ref={imgref.current[i]} src="" />
+          <img
+            key={v}
+            ref={(el) => (imgref.current[i] = el as HTMLImageElement)}
+            src=""
+          />
           CAMERA{i}
         </div>
       ))}
